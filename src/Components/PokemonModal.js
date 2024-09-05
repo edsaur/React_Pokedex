edc69e877318ae4da6efe527cf8d92d246928302
Edzer Dionido <jeddionido26@gmail.com>
2024-09-05 11:43:46 +0800

    fix code

--- src/Components/PokemonModal.js
+++ src/Components/PokemonModal.js
@@ -1,15 +1,16 @@
 import './modal.css';
 
 export default function PokemonModal({pokemon, onModalToggle, isLiked, onLikedPokemon}) {
+  console.log(pokemon);
     return (
       <div className='modal'>
         <div className="overlay" onClick={onModalToggle}></div>
         <div className='modal-content'>
-          <img src={pokemon.image} alt={pokemon.name} />
-          <h1>{pokemon.name}</h1>
+          <img src={pokemon.img} alt={pokemon.name} />
+          <h1>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1).toLowerCase()}</h1>
           <div>
             <h2>Pokemon Type:</h2>
-            {pokemon.type.map(type => <h3 className='type' key={type}>{type}</h3>)}
+            {pokemon.types.map(type => <h3 className='type' key={type.slot}>{type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1).toLowerCase()}</h3>)}
           </div>
           <button className="heart-button" onClick={onLikedPokemon}>
             {isLiked ? "❤️" : "♡"}
